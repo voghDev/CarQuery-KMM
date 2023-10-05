efccb21c611bc75bc39b848e1b2b12428291d590
Olmo Gallegos <vendofiatpunto@hotmail.com>
2023-10-05 14:31:57 +0200

    Add first deep link

--- app/src/main/java/com/example/carquerykmm/MainActivity.kt
+++ app/src/main/java/com/example/carquerykmm/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.carquerykmm
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,6 +13,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.example.carquerykmm.ui.theme.CarQueryKMMTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +24,12 @@ class MainActivity : ComponentActivity() {
             CarQueryKMMTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Greeting {
+                        val deepLink = Uri.parse("domestika://mycourses")
+                        val intent = Intent(Intent.ACTION_VIEW, deepLink)
+
+                        startActivity(intent)
+                    }
                 }
             }
         }
@@ -27,17 +37,11 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(modifier: Modifier = Modifier, onClick: () -> Unit) {
     Text(
-            text = "Hello $name!",
-            modifier = modifier
+            text = "Click here to visit Domestika Deeplink",
+            modifier = modifier.clickable {
+                onClick()
+            },
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CarQueryKMMTheme {
-        Greeting("Android")
-    }
 }
