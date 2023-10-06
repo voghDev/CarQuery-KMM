2015de8ae6a452168d6c2fc34fc30b6b34c548db
Olmo Gallegos <vendofiatpunto@hotmail.com>
2023-10-06 13:11:27 +0200

    All deep links added

--- app/src/main/java/com/example/carquerykmm/MainActivity.kt
+++ app/src/main/java/com/example/carquerykmm/MainActivity.kt
@@ -5,21 +5,19 @@ import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
+import androidx.compose.ui.graphics.Color
 import com.example.carquerykmm.ui.theme.CarQueryKMMTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,11 +30,25 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Column(Modifier.fillMaxSize()) {
+                    Column(
+                        Modifier
+                            .fillMaxSize()
+                            .verticalScroll(rememberScrollState())
+                    ) {
                         DeepLinkButton("My Courses DeepLink") { launchDeepLink("domestika://mycourses") }
                         DeepLinkButton("Course Landing DeepLink") { launchDeepLink("domestika://course") }
                         DeepLinkButton("Courses DeepLink") { launchDeepLink("domestika://courses/") }
                         DeepLinkButton("Profile DeepLink") { launchDeepLink("domestika://profile/") }
+                        DeepLinkButton("ConfirmRegistration DeepLink") { launchDeepLink("domestika://apps") }
+                        DeepLinkButton("Downloads DeepLink") { launchDeepLink("domestika://downloads") }
+                        DeepLinkButton("Login DeepLink") { launchDeepLink("domestika://login") }
+                        DeepLinkButton("My Settings DeepLink") { launchDeepLink("domestika://mysettings") }
+                        DeepLinkButton("Project DeepLink") { launchDeepLink("domestika://project/12") }
+                        DeepLinkButton("SignUp DeepLink") { launchDeepLink("domestika://signup") }
+                        DeepLinkButton("ResetPassword DeepLink") { launchDeepLink("domestika://auth/password/edit") }
+                        DeepLinkButton("Search DeepLink") { launchDeepLink("domestika://search") }
+                        DeepLinkButton("Topic DeepLink") { launchDeepLink("domestika://topic/") }
+                        DeepLinkButton("Topic Detail DeepLink") { launchDeepLink("domestika://topic/12") }
                     }
                 }
             }
@@ -55,7 +67,9 @@ class MainActivity : ComponentActivity() {
 fun DeepLinkButton(text: String, onClick: () -> Unit) {
     Button(
         modifier = Modifier.wrapContentSize(),
-        onClick = { onClick() }) {
+        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFF02D00)),
+        onClick = { onClick() }
+    ) {
         Text(text = text)
     }
 }
