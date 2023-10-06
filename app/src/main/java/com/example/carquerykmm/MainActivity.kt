7301eebcf594534c68b30957139030b819268ec1
Olmo Gallegos <vendofiatpunto@hotmail.com>
2023-10-06 12:20:15 +0200

    Add 4 deep links

--- app/src/main/java/com/example/carquerykmm/MainActivity.kt
+++ app/src/main/java/com/example/carquerykmm/MainActivity.kt
@@ -6,7 +6,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -23,25 +28,34 @@ class MainActivity : ComponentActivity() {
         setContent {
             CarQueryKMMTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting {
-                        val deepLink = Uri.parse("domestika://mycourses")
-                        val intent = Intent(Intent.ACTION_VIEW, deepLink)
-
-                        startActivity(intent)
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column(Modifier.fillMaxSize()) {
+                        DeepLinkButton("My Courses DeepLink") { launchDeepLink("domestika://mycourses") }
+                        DeepLinkButton("Course Landing DeepLink") { launchDeepLink("domestika://course") }
+                        DeepLinkButton("Courses DeepLink") { launchDeepLink("domestika://courses/") }
+                        DeepLinkButton("Profile DeepLink") { launchDeepLink("domestika://profile/") }
                     }
                 }
             }
         }
     }
+
+    private fun launchDeepLink(uri: String) {
+        val deepLink = Uri.parse(uri)
+        val intent = Intent(Intent.ACTION_VIEW, deepLink)
+
+        startActivity(intent)
+    }
 }
 
 @Composable
-fun Greeting(modifier: Modifier = Modifier, onClick: () -> Unit) {
-    Text(
-            text = "Click here to visit Domestika Deeplink",
-            modifier = modifier.clickable {
-                onClick()
-            },
-    )
+fun DeepLinkButton(text: String, onClick: () -> Unit) {
+    Button(
+        modifier = Modifier.wrapContentSize(),
+        onClick = { onClick() }) {
+        Text(text = text)
+    }
 }
