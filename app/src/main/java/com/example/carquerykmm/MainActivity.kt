f40984c346ec73792bbe8633eecd91f209cd9e58
Olmo Gallegos <olmo.gallegos@ext.domestika.org>
2023-10-23 18:18:15 +0200

    chore: More deep links

--- app/src/main/java/com/example/carquerykmm/MainActivity.kt
+++ app/src/main/java/com/example/carquerykmm/MainActivity.kt
@@ -37,9 +37,11 @@ class MainActivity : ComponentActivity() {
                     ) {
                         val scheme = BuildConfig.Scheme
                         DeepLinkButton("login") { launchDeepLink("$scheme://login") }
+                        DeepLinkButton("apps") { launchDeepLink("$scheme://apps") }
                         DeepLinkButton("downloads") { launchDeepLink("$scheme://downloads") }
                         DeepLinkButton("mycourses") { launchDeepLink("$scheme://mycourses") }
                         DeepLinkButton("myprofile") { launchDeepLink("$scheme://myprofile") }
+                        DeepLinkButton("mysettings") { launchDeepLink("$scheme://mysettings") }
                         DeepLinkButton("Courses orderBy popular 11") { launchDeepLink("$scheme://courses?order=popular&filters=exclude_purchased&filterParams=11") }
                         DeepLinkButton("Courses orderBy popular") { launchDeepLink("$scheme://courses?order=popular&filters=exclude_purchased") }
                         DeepLinkButton("Courses orderBy recent 11") { launchDeepLink("$scheme://courses?order=recent&filters=exclude_purchased&filterParams=11") }
@@ -49,7 +51,23 @@ class MainActivity : ComponentActivity() {
                         DeepLinkButton("Courses orderBy highestRated 11") { launchDeepLink("$scheme://courses?order=highest_rated&filters=exclude_purchased&filterParams=11") }
                         DeepLinkButton("Courses orderBy highestRated") { launchDeepLink("$scheme://courses?order=highest_rated&filters=exclude_purchased") }
                         DeepLinkButton("Courses 528") { launchDeepLink("$scheme://courses/528") }
+                        DeepLinkButton("Courses") { launchDeepLink("$scheme://courses/") }
                         DeepLinkButton("search watercolor") { launchDeepLink("$scheme://search/watercolor") }
+                        DeepLinkButton("SignUp DeepLink") { launchDeepLink("$scheme://signup") }
+                        DeepLinkButton("ResetPassword DeepLink") { launchDeepLink("$scheme://auth/password/edit") }
+                        DeepLinkButton("Search DeepLink") { launchDeepLink("$scheme://search") }
+                        DeepLinkButton("Topic DeepLink") { launchDeepLink("$scheme://topic/") }
+                        DeepLinkButton("Launch DeepLink") { launchDeepLink("$scheme://launch/") }
+                        DeepLinkButton("Topic Detail DeepLink") { launchDeepLink("$scheme://topic/12") }
+                        DeepLinkButton("Project Detail DeepLink") { launchDeepLink("$scheme://project/1666844") }
+                        DeepLinkButton("exclude_purchases") { launchDeepLink("$scheme://courses?filter=exclude_purchases")}
+                        DeepLinkButton("purchased_only") { launchDeepLink("$scheme://courses?filter=purchased_only")}
+                        DeepLinkButton("accessible_only") { launchDeepLink("$scheme://courses?filter=accessible_only")}
+                        DeepLinkButton("exclude_accessible") { launchDeepLink("$scheme://courses?filter=exclude_accessible")}
+                        DeepLinkButton("basics") { launchDeepLink("$scheme://courses?filter=basics")}
+                        DeepLinkButton("category") { launchDeepLink("$scheme://courses?filter=category")}
+                        DeepLinkButton("area") { launchDeepLink("$scheme://courses?filter=area")}
+                        DeepLinkButton("Lesson") { launchDeepLink("$scheme://courses/4855/units/16672/lessons/53071")}
                     }
                 }
             }
